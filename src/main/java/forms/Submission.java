1dbda54990a3a261bb987188dc0d782f899bb2f0
Elias Storme <elias.storme@gmail.com>
2017-06-02 20:33:55 +0200

    Initial commit

---
+++ src/main/java/forms/Submission.java
@@ -0,0 +1,14 @@
+package forms;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by elias on 02/06/17.
+ */
+public class Submission {
+
+    private String content;
+
+    private LocalDateTime submissionTime;
+
+}
