9eb7a314b4df45fd73f7d7b7ce0dceb95f357506
Elias Storme <elias.storme@gmail.com>
2017-06-03 22:49:32 +0200

    Added method to extract initials from string of name, made test for it.

--- src/main/java/forms/Submission.java
+++ src/main/java/forms/Submission.java
@@ -1,27 +1,77 @@
 package forms;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.omg.CORBA.DynAnyPackage.Invalid;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * Created by elias on 02/06/17.
  */
 public class Submission {
 
-    private String content;
-
-    private LocalDateTime submissionTime;
-
-    public Submission(LocalDateTime submissionTime, String content){
-
+    public Submission(LocalDateTime submissionTime, String content) {
+        this.content = content;
+        this.submissionTime = submissionTime;
     }
 
-    public String generateInitials(String input){
+    /******************************************
+     * Submission time
+     ******************************************/
+
+    private final LocalDateTime submissionTime;
+
+    public LocalDateTime getSubmissionTime(){
+        return this.submissionTime;
+    }
+
+    /******************************************
+     * Content
+     ******************************************/
+
+    private String content;
+
+    /**
+     * @return this submissions content.
+     */
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /******************************************
+     * Name censoring
+     ******************************************/
+
+    public String censorName(String input) {
+        if (input == null) throw new NullPointerException("String is null!");
+        else if (input.length() == 0) throw new IllegalArgumentException("String is empty!");
+
+        String names[] = input.split(" ");
+        String initials = "";
+
+        for (String name : names) {
+            if(name.isEmpty()) continue;
+            initials += generateInitials(name);
+            initials += ".";
+        }
+
+        return initials;
+    }
+
+    public String generateInitials(String input) {
         input = input.trim();
         char initial = input.charAt(0);
-        if(Character.isLowerCase(initial)){
+        if (Character.isLowerCase(initial)) {
             initial = Character.toUpperCase(initial);
         }
         return new String(new char[]{initial});
     }
+
 
 }
---
+++ src/test/java/forms/SubmissionTest.java
@@ -0,0 +1,33 @@
+package forms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by elias on 03/06/17.
+ */
+public class SubmissionTest {
+
+    Submission standardSub;
+
+    @Before
+    public void setUp() {
+        standardSub = new Submission(LocalDateTime.now(), "bla die bla");
+    }
+
+    @Test
+    public void generateInitialsTest() {
+        assertEquals("E", standardSub.generateInitials("       elias  "));
+    }
+
+    @Test
+    public void censorNameTest(){
+        assertEquals("A.M.C.", standardSub.censorName(" ava   mirzaee  cheshmeh"));
+        System.out.println(standardSub.censorName(" ava   mirzaee  cheshmeh"));
+    }
+
+}
